c8c6f9d4623026558279bf684488dac588096cee
Kiran Welle <me@kiranwelle.com>
2017-06-21 11:29:05 -0700

    Add temporary splash page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,198 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
+
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Where You Left Off</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+        
+        <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/img/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/img/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/img/favicon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/img/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+        
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
+            .page-header {
+                margin-bottom: 0px;
             }
-
-            .full-height {
-                height: 100vh;
+            .page-header img {
+                height: 90px;
             }
-
-            .flex-center {
-                align-items: center;
+            .vertical-center {
+                min-height: 75%;  /* Fallback for browsers do NOT support vh unit */
+                min-height: 75vh; /* These two lines are counted as one :-)       */
+                margin-bottom: 0px;
                 display: flex;
-                justify-content: center;
+                align-items: center;
             }
+            .mailing {
+                margin-top: 50px;
+            }
+			
+			.social {
+				margin-top: 50px;
+			}
+			
+			.center-block {  
+				width:325px;  
+				padding:10px;  
+				background-color:#eceadc;  
+				color:#ec8007  
+			}  
 
-            .position-ref {
-                position: relative;
-            }
+			
+            @font-face {
+				font-family: 'si';
+				src: url('font/socicon.eot');
+				src: url('font/socicon.eot?#iefix') format('embedded-opentype'),
+					 url('font/socicon.woff') format('woff'),
+					 url('font/socicon.ttf') format('truetype'),
+					 url('font/socicon.svg#icomoonregular') format('svg');
+				font-weight: normal;
+				font-style: normal;
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+			}
 
-            .content {
-                text-align: center;
-            }
+			@media screen and (-webkit-min-device-pixel-ratio:0) {
+				@font-face {
+					font-family:si;
+					src: url(font/socicon.svg) format(svg);
+				}
+			}
 
-            .title {
-                font-size: 84px;
-            }
+			.soc {
+				overflow:hidden;
+				margin:0; padding:0;
+				list-style:none;
+				text-align:center;
+			}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+			.soc li {
+				display:inline-block;
+				*display:inline;
+				zoom:1;
+			}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+			.soc li a {
+				font-family:si!important;
+				font-style:normal;
+				font-weight:400;
+				-webkit-font-smoothing:antialiased;
+				-moz-osx-font-smoothing:grayscale;
+				-webkit-box-sizing:border-box;
+				-moz-box-sizing:border-box;
+				-ms-box-sizing:border-box;
+				-o-box-sizing:border-box;
+				box-sizing:border-box;
+
+				overflow:hidden;
+				text-decoration:none;
+				text-align:center;
+				display:block;
+				position: relative;
+				z-index: 1;
+				width: 47px;
+				height: 47px;
+				line-height: 47px;
+				font-size: 25px;
+				-webkit-border-radius: 10px;
+				-moz-border-radius: 10px;
+				border-radius: 10px;
+				margin-right: 10px;
+				color: #ffffff;
+				background-color: none;
+			}
+
+
+			.soc-icon-last{
+				margin:0 !important;
+			}
+
+			.soc-twitter {
+				background-color: #4da7de;
+			}
+			.soc-twitter:before {
+				content:'\e040';
+			}
+			.soc-instagram {
+				background-color: #405de6;
+			}
+			.soc-instagram:before {
+				content:'\e057';
+			}
+			.soc-tumblr {
+				background-color: #45556c;
+			}
+			.soc-tumblr:before {
+				content:'\e059';
+			}
+			.soc-facebook {
+				background-color: #3e5b98;
+			}
+			.soc-facebook:before {
+				content:'\e041';
+			}
+			.soc-github {
+				background-color: #221e1b;
+			}
+			.soc-github:before {
+				content:'\e030';
+			}
         </style>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+        <div class="jumbotron vertical-center">
+            <div class="container">
+                <div class="page-header text-center">
+                    <h1>Where You Left Off</h1>
+                    <h1><img src="img/wylo_icons.png"></h1>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="row">
+                    <div class="col-lg-12 text-center"><h3>Getting back to the story</h3></div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="row">
+                    <div class="col-lg-12 text-center"><h3>Coming Soon</h3></div>
                 </div>
+				<div class="row social">
+					<ul class="soc">
+						<!-- http://perfecticons.com/ -->
+						<li><a class="soc-twitter" href="https://www.twitter.com/WhereYouLeftOff"></a></li>
+						<li><a class="soc-instagram" href="https://www.instagram.com/WhereYouLeftOff"></a></li>
+						<li><a class="soc-tumblr" href="http://blog.whereyouleftoff.com"></a></li>
+						<li><a class="soc-facebook" href="https://www.facebook.com/WhereYouLeftOff"></a></li>
+						<li><a class="soc-github soc-icon-last" href="https://github.com/Worthwelle/WhereYouLeftOff"></a></li>
+					</ul>
+				</div>
             </div>
         </div>
+
+        <footer class="footer">
+            <div class="container">
+                <p class="text-center text-muted"><small>Where You Left Off &copy; 2017 Worthwelle Design</small></p>
+            </div>
+        </footer>
     </body>
 </html>
